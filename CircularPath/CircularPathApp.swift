548b3f170da93ef85e757cf08f78747a74769724
Christopher Alford <chrisvalford@me.com>
2021-11-03 17:12:22 +0100

    Initial Commit

---
+++ CircularPath/CircularPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularPathApp.swift
+//  CircularPath
+//
+//  Created by Christopher Alford on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CircularPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircularPath/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CircularPath
+//
+//  Created by Christopher Alford on 3/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
